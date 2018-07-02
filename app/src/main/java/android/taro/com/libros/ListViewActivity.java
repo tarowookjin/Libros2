1891bd606224f982e301276a8ec669525fde532f
Taro <tarowj05@gmail.com>
2018-07-01 21:47:13 -0500

    initial Commit

---
+++ app/src/main/java/android/taro/com/libros/ListViewActivity.java
@@ -0,0 +1,79 @@
+package android.taro.com.libros;
+
+import java.util.ArrayList;
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ListViewActivity extends Activity {
+
+    SQLiteHelper SQLITEHELPER;
+    SQLiteDatabase SQLITEDATABASE;
+    Cursor cursor;
+    SQLiteListAdapter ListAdapter ;
+
+    ArrayList<String> ID_ArrayList = new ArrayList<String>();
+    ArrayList<String> NAME_ArrayList = new ArrayList<String>();
+    ArrayList<String> PHONE_NUMBER_ArrayList = new ArrayList<String>();
+    ArrayList<String> SUBJECT_ArrayList = new ArrayList<String>();
+    ListView LISTVIEW;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        LISTVIEW = (ListView) findViewById(R.id.listView1);
+
+        SQLITEHELPER = new SQLiteHelper(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+
+        ShowSQLiteDBdata() ;
+
+        super.onResume();
+    }
+
+    private void ShowSQLiteDBdata() {
+
+        SQLITEDATABASE = SQLITEHELPER.getWritableDatabase();
+
+        cursor = SQLITEDATABASE.rawQuery("SELECT * FROM demoTable", null);
+
+        ID_ArrayList.clear();
+        NAME_ArrayList.clear();
+        PHONE_NUMBER_ArrayList.clear();
+        SUBJECT_ArrayList.clear();
+
+        if (cursor.moveToFirst()) {
+            do {
+                ID_ArrayList.add(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_ID)));
+
+                NAME_ArrayList.add(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_Name)));
+
+                PHONE_NUMBER_ArrayList.add(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_PhoneNumber)));
+
+                SUBJECT_ArrayList.add(cursor.getString(cursor.getColumnIndex(SQLiteHelper.KEY_Subject)));
+
+            } while (cursor.moveToNext());
+        }
+
+        ListAdapter = new SQLiteListAdapter(ListViewActivity.this,
+
+                ID_ArrayList,
+                NAME_ArrayList,
+                PHONE_NUMBER_ArrayList,
+                SUBJECT_ArrayList
+
+        );
+
+        LISTVIEW.setAdapter(ListAdapter);
+
+        cursor.close();
+    }
+}
